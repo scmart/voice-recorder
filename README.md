0d70fa47337947585263173a949f020114e4830f
Sedric <sedric@torsh.co>
2014-04-15 18:56:57 -0700

    Voice Recorder v0.1

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 voice-recorder
 ==============
+
+Version .1
+
+
+Voice Recorder is built upon:
+
+Wami Recorder (for Flash fallback)
+https://code.google.com/p/wami-recorder/
+
+and 
+
+Recorderjs
+https://github.com/mattdiamond/Recorderjs
+
+
+
+
+Voice Recorder provides a simple voice recorder solution with a flash fallback for older browsers that don't support the Web Audo API's.  Voicerecorder requires the underscrore framework (you could remove that requirement with very little work if you dont already use underscore).  The was primarily designed to work with the Meteor framework, but it will work just fine anywhere.
+
+
+To use Voice Recorder, include the following files:
+
+<script type="text/javascript" src="recorderjs/recorder.js"></script>
+<script type="text/javascript" src="wami/swfobject/swfobject.js"></script>
+<script type="text/javascript" src="wami/recorder.js"></script>
+<script type="text/javascript" src="voicerecord.js"></script>
+
+
+Then create and setup a voicerecorder object:
+
+var vr = new VoiceRecorder();
+
+vr.setup();
+
+
+Then you can start recording with:
+
+vr.start();
+
+
+And stop with:
+
+vr.stop();
+
+
+Voicerecorder passes the audio data back to a the callback onFinish, that can be configured in two ways:
+
+First you can include the callback in a call to setup:
+
+vr.setup({callbacks: {onFinish: function(data) {...}}});
+
+
+second you can pass the callback to the start function:
+
+vr.start({onFinish: function(data) {...}});
+
---
+++ voicerecord.js
@@ -0,0 +1,169 @@
+(function(window, _) {
+	var VoiceRecorder = function() {
+		this._sampleRate = 16000;
+		this._flashSetupParams = {
+			id: 'flashRecorder',
+			swfUrl: 'wami/Wami.swf'
+		};
+		this._flashParams = {
+			sampleRate: this._sampleRate,
+			passBack: true
+		};
+
+		this._callbacks = {
+			onReady: null,
+			onStart: null,
+			onFinish: null,
+			onError: null
+		};
+		
+		this._flashReady = false;
+		this._flash = window.Wami;
+
+		this._recording = false;
+	};
+
+	VoiceRecorder.prototype.setup = function(params) {
+		var useFlash = this._useFlash;
+
+		//test for and shim for webkit native audio support
+		window.AudioContext = window.AudioContext || window.webkitAudioContext;
+		navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+ 
+		if (window.AudioContext && navigator.getUserMedia) {
+			useFlash = false;
+		} else {
+			useFlash = true;
+		}
+
+		if (params) {
+			if (params.callbacks) {
+				_.extend(this._callbacks, params.callbacks);
+			}
+			if (params.flashDiv) {
+				_.extend(this._flashSetupParams, {
+					id: params.flashDiv
+				});
+			}
+			if (params.flashURL) {
+				_.extend(this._flashSetupParams, {
+					swfUrl: params.flashURL
+				});
+			}
+			if (params.forceNative) {
+				useFlash = false;
+			}
+			if (params.forceFlash) {
+				useFlash = true;
+			}
+			if (params.flashParams) {
+				_.extend(this._flashParams, params.flashParams);
+			}
+			if (params.sampleRate) {
+				this._sampleRate = params.sampleRate;
+				this._flashParams.sampleRate = params.sampleRate;
+			}
+			if (params.onReady) {
+				this._callbacks.onReady = params.onReady;
+			}
+		}
+
+		if (this._useFlash) {
+			if (!useFlash) {
+				//switch to not using flash
+				//in case we are switching from flash to not
+				this._flashReady = false;
+				this._flash.cleanup();
+
+				this._useFlash = false;
+
+				this._setupNative();
+			}
+		} else {
+			if (useFlash) {
+				// setup flash, was previously native
+				var postSetup = _.bind(function(){
+					this._flash.setSettings(this._flashParams);
+					this._useFlash = true;
+					this._flashReady = true;
+					if (this._callbacks.onReady) {
+						this._callbacks.onReady();
+					}
+				}, this);
+				
+				this._flash.setup(_.extend(this._flashSetupParams, {
+					onReady: postSetup
+				}));
+								  
+			} else {
+				// first time setup for native audio
+				this._setupNative();
+				this._useFlash = false;
+				if (this._callbacks.onReady) {
+					this._callbacks.onReady();
+				}
+			}
+		}
+	};
+
+	VoiceRecorder.prototype._setupNative = function() {
+		navigator.getUserMedia({audio: true}, _.bind(function(stream) {
+			//setup native recording
+			var audio_context = new AudioContext();
+			
+			var input = audio_context.createMediaStreamSource(stream);
+			//input.connect(audio_context.destination);
+			
+			this._native = new Recorder(input);
+		}, this));
+	};
+
+	VoiceRecorder.prototype._decode64 = function() {
+		
+	};
+
+	VoiceRecorder.prototype.start = function(callbacks) {
+		if (callbacks) {
+			_.extend(this._callbacks, callbacks);
+		}
+		if (!this._recording) {
+			if (this._useFlash) {
+				if (this._flashReady) {
+					var Wami = this._flash;
+					Wami.startRecording("",
+										Wami.nameCallback(this._callbacks.onStart),
+										Wami.nameCallback(_.bind(function(data) {
+											this._callbacks.onFinish(data[0]);
+										},this)),
+										Wami.nameCallback(this._callbacks.onError)
+									   );
+					this._recording = true;
+				}
+			} else {
+				if (this._native) {
+					this._native.record();
+					this._recording = true;
+					this._callbacks.onStart();
+				}
+			}
+		}
+	};
+
+	VoiceRecorder.prototype.stop = function() {
+		var callback = this._callback;
+		if (this._recording) {
+			if (this._useFlash) {
+				this._flash.stopRecording();
+				this._recording = false;
+			} else {
+				this._native.stop();
+				this._recording = false;
+				this._native.exportWAV(_.bind(function(data) {
+					this._callbacks.onFinish(data);
+					this._native.clear();
+				}, this));
+			}
+		}
+	};
+	window.VoiceRecorder = VoiceRecorder;
+})(window, _);
